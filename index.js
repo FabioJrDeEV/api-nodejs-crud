fe6f669ce6d192b2f02d06a77db0e8c4d5209d93
FabioJrDeEV <fj453578@gmail.com>
2025-06-14 10:53:19 -0300

    feat: ajuste na rota post

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ app.post("/tasks", async (req, res) => {
   const { title, description, day, completed } = req.body;
 
   try {
-    await pool.query(
+    const resultado = await pool.query(
       "INSERT INTO banco_do_crud (title, description, day, completed) VALUES ($1, $2, $3, $4)",
       [title, description, day, completed]
     );
-    res.status(201).json();
+    res.status(201).json(resultado.rows[0]);
   } catch (err) {
     console.log(err);
     res.status(500).send("Erro ao adicionar task");
