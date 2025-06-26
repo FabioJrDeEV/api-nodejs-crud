cfeff1799084714b172ea376b40ed2474f1f8347
FabioJrDeEV <fj453578@gmail.com>
2025-06-25 22:07:12 -0300

    ajustando o auth

--- auth/auth.js
+++ auth/auth.js
@@ -21,6 +21,7 @@ router.post(
   ],
   async (req, res) => {
     const { email, password } = req.body;
+
     const erros = validationResult(req);
     if (!erros.isEmpty()) {
       return res.status(400).json({ erros: erros.array() });
